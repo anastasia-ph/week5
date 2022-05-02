106f5ffe22789a1494f45c8862720b82c0fa45d6
Anastasia <uranusinfection@gmail.com>
2022-05-02 22:48:07 +0200

    HW 27 cards

---
+++ hw27/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from "./components/Card"
+import style1 from "./components/card1.module.css"
+import style2 from "./components/card2.module.css"
+import style3 from "./components/card3.module.css"
+import style4 from "./components/card4.module.css"
+import data from "./components/data.json"
+
+
+function App() {
+  return (
+<div className='container'>
+  <Card style = {style1} header={data[0].header} currencytype={data[0].currency} currency={data[0].value} period={data[0].term} connection={data[0].speed} traffictype={data[0].traffic}/>
+   <Card style = {style2} header={data[1].header} currencytype={data[1].currency} currency={data[1].value} period={data[1].term} connection={data[1].speed} traffictype={data[1].traffic}/>
+   <Card Active style = {style3} header={data[2].header} currencytype={data[2].currency} currency={data[2].value} period={data[2].term} connection={data[2].speed} traffictype={data[2].traffic}/>
+   <Card style = {style4} header={data[3].header} currencytype={data[3].currency} currency={data[3].value} period={data[3].term} connection={data[3].speed} traffictype={data[3].traffic}/>
+   </div>
+  )
+}
+
+export default App;
---
+++ hw27/src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Card(props) {
+    const style = props.style;
+    let cardBody;
+
+    if (props.Active) {
+        cardBody = style.card_selected
+    }
+    else {
+        cardBody = style.card
+    }
+
+    return (
+
+        < div className={cardBody}>
+            <div className={style.card__header} >{props.header}</div>
+            <div className={style.card__body}>
+                <div className={style.card__price_block}>
+                    <p className={style.card__body_text_flex_start} >{props.currencytype}</p>
+                    <p className={style.card__body_large_text}>{props.currency}</p>
+                    <p className={style.card__body_text}>{props.period}</p>
+                </div>
+                <div className={style.card__connectiontype}>{props.connection}</div>
+            </div>
+
+            <div className={style.card__footer}>{props.traffictype}</div>
+        </ div>
+    )
+}
+
+export default Card;
